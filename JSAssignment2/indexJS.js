91df081fb59d02d723a4086acc484207ff87f5b7
bhramarv <bhramarvirmani@fareportal.com>
2022-02-17 00:27:31 +0530

    Added Assignment 2 MouseOver And Out Assignment

---
+++ JSAssignment2/indexJS.js
@@ -0,0 +1,46 @@
+function onMouseDiv1(){
+  // let h1tag = document.getElementsByClassName("div1");
+  // this.style.
+  // let h1tag = document.getElementsByClassName("div1");
+  // document.getElementById("div1").style.backgroundColor='yellow';
+  // document.getElementById("div1").style.border='dotted';
+  document.getElementById("div1").style.color="red";
+}
+
+function onMouseDiv2(){
+  // document.getElementById("div2").style.backgroundColor='yellow';
+  // document.getElementById("div2").style.border='dotted';
+  document.getElementById("div2").style.color="red"
+}
+
+function onDefaultDiv1(){
+  // document.getElementById("div1").style.border="";
+  // document.getElementById("div1").style.backgroundColor='white';
+  document.getElementById("div1").style.color="black";
+}
+
+function onDefaultDiv2(){
+  // document.getElementById("div2").style.border="";
+  // document.getElementById("div2").style.backgroundColor='white';
+  document.getElementById("div2").style.color="black";
+}
+
+document.getElementById("DivTag1")
+    .onmouseover = function () {
+        document.getElementById("DivTag1").style.color="yellow";
+};
+
+document.getElementById("DivTag1")
+    .onmouseout = function () {
+        document.getElementById("DivTag1").style.color="black";
+};
+    
+document.getElementById("DivTag2")
+    .onmouseover = function () {
+        document.getElementById("DivTag2").style.color="yellow";
+};
+
+document.getElementById("DivTag2")
+    .onmouseout = function () {
+        document.getElementById("DivTag2").style.color="black";
+};
---
+++ RandomNum.js
@@ -0,0 +1,7 @@
+for (let i=0; i<10; i++){
+var num = Math.floor(Math.random() * 20) + 1;
+const a = Math.floor(Math.random() * (20 + 1)) + 0;
+console.log(num);
+console.log(a);
+console.log("\n");
+}
